bf339fac6bbcb0225a0b493d037cc531b1483f6c
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-09-30 15:12:01 +0530

    Modified

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,11 @@
-const registerUser = async (req,res) => {
+const registerUser =  (req,res) => {
+    if(!req.body.email){
+        res.status(400)
+        throw new Error("Please add your email");
+    }
     res.send("Register User")
 };
 
+module.exports = {
+    registerUser,
+};
