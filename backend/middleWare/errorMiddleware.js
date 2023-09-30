4b8021bb5bc2952097effd72a43f0e528eb84ab0
Saman Kumara <samankumararajapaksha660@gmail.com>
2023-09-30 16:40:19 +0530

    modified register page

--- backend/middleWare/errorMiddleware.js
+++ backend/middleWare/errorMiddleware.js
@@ -3,7 +3,10 @@ const errorHandler = (err, req, res, next) => {
   
     res.status(statusCode);
   
-    
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === "development" ? err.stack : null,
+      });
   };
   
   module.exports = errorHandler;
